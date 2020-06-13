33bc79352c9ef0922d9e7838f126f9bb0f319718
sidhant <sidhantsharma293@gmail.com>
2020-06-13 16:53:09 +0530

    Updates

---
+++ precache-manifest.63a6e7b7e271adeabd16c923274afdf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b0cc6861198066eb0c80def527a5c6",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/Robo-Friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "00a0ad8f7dac1d294480",
+    "url": "/Robo-Friends/static/css/main.fa618787.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/Robo-Friends/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Friends/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a0ad8f7dac1d294480",
+    "url": "/Robo-Friends/static/js/main.e007b66f.chunk.js"
+  },
+  {
+    "revision": "fb6474ec5898eda23474",
+    "url": "/Robo-Friends/static/js/runtime-main.cc365db8.js"
+  }
+]);
